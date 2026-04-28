3d1aacbb6ec9eb3e58cafaa6b62a04bc6083e874
Jessy <you@example.com>
2026-04-28 10:09:08 +0200

    Fix: Design page

--- components/Application/Dashboard/WaitingRdv.tsx
+++ components/Application/Dashboard/WaitingRdv.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import { toast } from "sonner";
 
 import {
+  confirmAppointmentAction,
   paidAppointmentsAction,
   waitingConfirmationAppointmentsAction,
 } from "@/lib/queries/appointment";
